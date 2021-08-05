328f6a67a4d947a30aa1c3ee5fce2623163cdc1c
pawankumarmahto <mahtokumarpawan0307@gmail.com>
2021-08-05 22:51:20 +0530

    Second Commit: deleted unused import

--- src/main/java/com/assignment/favourite/recipes/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/assignment/favourite/recipes/serviceImpl/UserServiceImpl.java
@@ -1,10 +1,7 @@
 package com.assignment.favourite.recipes.serviceImpl;
 
-import java.time.LocalDateTime;
-
 import org.springframework.stereotype.Service;
 
-import com.assignment.favourite.recipes.entity.Recipes;
 import com.assignment.favourite.recipes.entity.User;
 import com.assignment.favourite.recipes.repository.UserRepository;
 import com.assignment.favourite.recipes.service.UserService;
@@ -19,7 +16,7 @@ public class UserServiceImpl implements UserService {
 		User oldUser = userRepository.findByUserName(user.getUserName());
 		
 		if (oldUser != null ) {
-			return "Con not add, User is already exist";
+			return "Con not add, User is already exist.";
 		} else {
 			userRepository.save(user);
 			return "Recipe is saved successfully";
